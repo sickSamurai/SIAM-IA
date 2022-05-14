5ec8e3af296ebdfbe26aaadf55499b35b2f8c35e
Julian Pinzon <ldmckkb@gmail.com>
2022-05-14 12:53:30 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+from numpy.lib.polynomial import poly
+
+img = cv2.imread('images/6.jpg')
+box, label, count = cv.detect_common_objects(img)
+output = draw_bbox(img, box, label, count)
+output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(10, 10))
+plt.axis('off')
+plt.imshow(output)
+plt.show()
+print("Number of objects in this image are " + str(len(label)))
