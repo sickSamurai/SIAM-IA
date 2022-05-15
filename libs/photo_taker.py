ae884515fd6a9036f37b131d696b47fa93ac528a
Laurent <ldmckkb@gmail.com>
2022-05-14 23:31:27 -0500

    minor changes

--- libs/photo_taker.py
+++ libs/photo_taker.py
@@ -8,8 +8,8 @@ def take_photo(route_name=""):
     # configure for take photos periodically
     threading.Timer(30, take_photo, [route_name]).start()
     # initialize the camera
-    cam = cv2.VideoCapture(0)  # 0 -> index of camera
-    result, image = cam.read()
+    camera = cv2.VideoCapture(0)  # 0 -> index of camera
+    result, image = camera.read()
     if result:
         current_directory = os.getcwd()
         cv2.imwrite("{}/images/{}.jpg".format(current_directory, route_name), image)
