190fe5d9cc4201c0a7f32a05c685a6b87c75e4f2
Laurent <ldmckkb@gmail.com>
2022-05-15 07:12:49 -0500

    imports optimized

--- libs/person_detector.py
+++ libs/person_detector.py
@@ -1,11 +1,8 @@
 import cv2
-import json
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
 import numpy as np
 import matplotlib.pyplot as plt
-import cvlib as cv
-
-import platform
-from cvlib.object_detection import draw_bbox
 from numpy.lib.polynomial import poly
 
 
@@ -14,6 +11,3 @@ def detect_persons(route_name=""):
     box, labels, count = cv.detect_common_objects(img, confidence=0)
     person_label_list = list(filter(lambda label: label == "person", labels))
     return len(person_label_list)
-
-
-
