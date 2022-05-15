8df4c21acf535c8a0e79f128add514fe56ddc797
Laurent <ldmckkb@gmail.com>
2022-05-14 23:11:56 -0500

    timer corrected

--- libs/person_detector.py
+++ libs/person_detector.py
@@ -11,19 +11,15 @@ from numpy.lib.polynomial import poly
 def detect_persons(route_name=""):
     img = cv2.imread("images/{}.jpg".format(route_name))
     box, labels, count = cv.detect_common_objects(img, confidence=0)
-
     person_label_list = list(filter(lambda label: label == "person", labels))
     return len(person_label_list)
 
 
 def generate_data_file(route_name=""):
-    threading.Timer(30, generate_data_file).start()
-    number_of_users = detect_persons(route_name)
-    route_data = {
+    threading.Timer(30, generate_data_file, [route_name]).start()
+    data_on_json = json.dumps({
         "name": route_name,
-        "numberOfUsers": number_of_users
-    }
-    data_on_JSON = json.dumps(route_data)
-    print(data_on_JSON)
+        "numberOfUsers": detect_persons(route_name)
+    })
     route_data_file = open("C:/SIAM-Data/routes-data.json", "w")
-    route_data_file.write(data_on_JSON)
+    route_data_file.write(data_on_json)
--- libs/photo_taker.py
+++ libs/photo_taker.py
@@ -1,15 +1,15 @@
+import os
+
 import cv2
 import threading
 
 
 def take_photo(route_name=""):
     # configure for take photos periodically
-    threading.Timer(30, take_photo).start()
-
+    threading.Timer(30, take_photo, [route_name]).start()
     # initialize the camera
     cam = cv2.VideoCapture(0)  # 0 -> index of camera
     result, image = cam.read()
     if result:
-        cv2.imwrite("images/{}.jpg".format(route_name), image)
-        print("Foto tomada con exito")
-
+        current_directory = os.getcwd()
+        cv2.imwrite("{}/images/{}.jpg".format(current_directory, route_name), image)
