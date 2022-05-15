5e6b0b5614cd107beec6a110550ade880bf0280b
Laurent <ldmckkb@gmail.com>
2022-05-15 07:43:02 -0500

    bug fixed, create dir if not exists

--- libs/routes_dao.py
+++ libs/routes_dao.py
@@ -1,12 +1,13 @@
+import os.path
 import threading
 import platform
 import json
 from libs import person_detector
 
 paths = {
-    "Windows": "C:/SIAM-Data/routes-data.json",
-    "Linux": "~/SIAM/routes-data.json",
-    "Darwin": "~/SIAM/routes-data.json"
+    "Windows": "C:/SIAM-Data",
+    "Linux": "~/SIAM",
+    "Darwin": "~/SIAM"
 }
 
 data_file_path = paths[platform.system()]
@@ -18,5 +19,7 @@ def generate_data_file(route_name=""):
         "name": route_name,
         "numberOfUsers": person_detector.detect_persons(route_name)
     })
-    route_data_file = open(data_file_path, "w")
+    if not os.path.exists(data_file_path):
+        os.mkdir(data_file_path)
+    route_data_file = open("{}/routes-data.json".format(data_file_path), "w")
     route_data_file.write(data_on_json)
